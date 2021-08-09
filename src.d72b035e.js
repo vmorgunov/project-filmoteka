81fb70e05547caef206e63adf45be581ddffeb2d
Vitalii Morgunov <utlab13@gmail.com>
2021-08-09 10:56:47 +0200

    Updates

--- src.d72b035e.js
+++ src.d72b035e.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/project-filmoteka/src.2e8d95a6.js.map
+//# sourceMappingURL=/project-filmoteka/src.d72b035e.js.map
